7ef35de824469e8a891b24cb42fd235d62f7b786
NAHIWIN007 <denz2885xy@gmail.com>
2025-01-03 11:53:26 +0700

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function Header() {
+    return(
+        <strong>
+            <div class="p-4 py-10 text-white bg-gradient-to-r from-indigo-500 to-teal-400">
+                <span class="text-4xl uppercase">Infomation technology</span>
+            </div>
+        </strong>
+    );
+}
